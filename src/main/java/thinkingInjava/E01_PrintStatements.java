1333231b5b4575bcb92ffad9e8417de066a40753
hike97 <seaatom@qq.com>
2020-03-09 23:10:15 +0800

    --init

---
+++ src/main/java/thinkingInjava/E01_PrintStatements.java
@@ -0,0 +1,15 @@
+package thinkingInjava;
+
+import net.mindview.util.Print;
+
+/**
+ * @author hike97 許せ　サスケ　これで最後だ
+ * @create 2020-03-09 22:41
+ * @desc
+ **/
+public class E01_PrintStatements {
+	public static void main (String[] args) {
+		System.out.println ("hello world");
+		Print.print ("hohoho");
+	}
+}
---
+++ src/main/java/thinkingInjava/TestJvaDoc.java
@@ -0,0 +1,22 @@
+package thinkingInjava;
+
+/** A class comment
+ * <pre>
+ * System.out.println(new Date());
+ * </pre>
+ */
+public class TestJvaDoc {
+		/** A variable comment */
+		public int i;
+		/** A method comment
+		 * You can <em>even</em> insert a list:
+		 * <ol>
+		 * <li> Item one
+		 * <li> Item two
+		 * <li> Item three
+		 Thinking in Java, 4 th Edition Annotated Solution Guide  18
+		 * </ol>
+		 */
+		public void f() {}
+	///:~
+}
